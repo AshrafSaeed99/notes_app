5b79083d2be09f57913faf4497f2b419558f3b86
Ashraf Saeed <dr.ashrafsaeed99@gmail.com>
2025-01-27 15:19:02 +0300

    register adaptor

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -5,4 +5,14 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  void addNote() {
+    emit(AddNoteLoading());
+    try {
+      // Add note logic here
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
 }
