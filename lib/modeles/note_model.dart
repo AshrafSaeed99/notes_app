c8151cf5dc394dc3aefadeaef276cb16239a2d0a
Ashraf Saeed <dr.ashrafsaeed99@gmail.com>
2025-07-11 18:24:58 +0300

    t4

---
+++ lib/modeles/note_model.dart
@@ -0,0 +1,6 @@
+class NoteModel {
+  final String title;
+  final String content;
+  final DateTime date;
+  NoteModel({required this.date, required this.title, required this.content});
+}
--- lib/widgets/note_widget.dart
+++ lib/widgets/note_widget.dart
@@ -5,22 +5,30 @@ class NoteWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Container(    
+      margin: const EdgeInsets.symmetric(vertical: 12),
+      padding: const EdgeInsets.all(12),
       decoration: BoxDecoration(
         color: Colors.white.withAlpha(40),
         borderRadius: BorderRadius.circular(12),
-      ),
-      width: double.infinity,
+      ),  
       height: 200,
-      margin: const EdgeInsets.symmetric(vertical: 12,),
-      child: ListTile(
-        isThreeLine: true,
-
-        title: Text('Note'),
-        subtitle: Text('This is a note'),
-        trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete),),
-        iconColor: Colors.red,
-        
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            Text("Note 1", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30,
+            //fontFamily: 'assets/fonts/Poppins-Regular.ttf',
+            ),),
+            IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red,))
+          ]),
+          SizedBox(height: 10,),
+          Text("Note 1", style: TextStyle(fontSize: 20),),
+          Spacer(),
+          Align(alignment: Alignment.bottomRight, child: Text('12/12/2022'))
+        ],
       ),
     );
   }
